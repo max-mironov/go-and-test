934f52830ff354c6404e6242d933d3a7fdfe59a3
Max Mironov <v-maximi@microsoft.com>
2020-03-18 12:16:26 +0300

    added port

--- main.go
+++ main.go
@@ -4,15 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
 	r.HandleFunc("/apple-app-site-association", AASAHandler)
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
